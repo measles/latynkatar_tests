c278fcde8d1b314231b08c7244172d6a652d3515
Andrej Zacharevicz <andrej@zahar.ws>
2025-04-21 17:30:20 +0200

    Add test on different convertion cases

--- python-pytest-selenium/tests/tools/test_converter.py
+++ python-pytest-selenium/tests/tools/test_converter.py
@@ -28,6 +28,9 @@ def get_ready_to_test_driver() -> Iterator[webdriver.Chrome]:
     driver.quit()
 
 
+#
+# Test on page view
+#
 def test_page_title(driver: webdriver.Chrome):
     """Test for title of a converter page.
 
@@ -37,6 +40,9 @@ def test_page_title(driver: webdriver.Chrome):
     assert driver.title == "Łatynkatar"
 
 
+#
+# Tests on text convertation
+#
 def test_convert_default(driver: webdriver.Chrome):
     """Test convertation with a default settings.
 
@@ -49,13 +55,89 @@ def test_convert_default(driver: webdriver.Chrome):
     )
     output_field = driver.find_element(by=By.ID, value="output")
 
-    input_field.send_keys(
-        "Варажылі важаняткі ў падножжацёмнай вежы, бо хацелі важаняткі адшукаць у лесе ежы."
-    )
+    input_field.send_keys("Хіба ж гэта шпакоўня? Смех ды й годзе!")
     convert_button.click()
     time.sleep(1)
 
     assert (
         output_field.get_attribute("value")
-        == "Varažyli važaniatki ŭ padnožžaciomnaj viežy, bo chacieli važaniatki adšukać u lesie ježy."
+        == "Chiba ž heta špakoŭnia? Śmiech dy j hodzie!"
+    )
+
+
+def test_convert_without_palatalization(driver: webdriver.Chrome):
+    """Test convertation without palatalization.
+
+    Args:
+        webdriver.Chrome: Chrome driver object
+    """
+    input_field = driver.find_element(by=By.ID, value="input")
+    convert_button = driver.find_element(
+        by=By.CSS_SELECTOR, value='input[title="Канвертаваць"]'
+    )
+    palatalization_check = driver.find_element(by=By.ID, value="palatalization")
+    output_field = driver.find_element(by=By.ID, value="output")
+
+    input_field.send_keys("Хіба ж гэта шпакоўня? Смех ды й годзе!")
+    palatalization_check.click()
+    convert_button.click()
+    time.sleep(1)
+
+    assert (
+        output_field.get_attribute("value")
+        == "Chiba ž heta špakoŭnia? Smiech dy j hodzie!"
+    )
+
+
+def test_convert_to_old_graphics(driver: webdriver.Chrome):
+    """Test convertation to an old graphics.
+
+    Args:
+        webdriver.Chrome: Chrome driver object
+    """
+    input_field = driver.find_element(by=By.ID, value="input")
+    old_graphics_button = driver.find_element(
+        by=By.CSS_SELECTOR, value='label[title="Старая лацінка"]'
+    )
+    convert_button = driver.find_element(
+        by=By.CSS_SELECTOR, value='input[title="Канвертаваць"]'
+    )
+    output_field = driver.find_element(by=By.ID, value="output")
+
+    input_field.send_keys("Хіба ж гэта шпакоўня? Смех ды й годзе!")
+    old_graphics_button.click()
+    convert_button.click()
+    time.sleep(1)
+
+    assert (
+        output_field.get_attribute("value")
+        == "Chiba ż heta szpakoŭnia? Śmiech dy j hodzie!"
+    )
+
+
+def test_convert_to_old_graphics_without_palatalization(driver: webdriver.Chrome):
+    """Test convertation to an old graphics without palatalization.
+
+    Args:
+        webdriver.Chrome: Chrome driver object
+    """
+    input_field = driver.find_element(by=By.ID, value="input")
+    old_graphics_button = driver.find_element(
+        by=By.CSS_SELECTOR, value='label[title="Старая лацінка"]'
+    )
+    palatalization_check = driver.find_element(by=By.ID, value="palatalization")
+    convert_button = driver.find_element(
+        by=By.CSS_SELECTOR, value='input[title="Канвертаваць"]'
+    )
+    output_field = driver.find_element(by=By.ID, value="output")
+
+    input_field.send_keys("Хіба ж гэта шпакоўня? Смех ды й годзе!")
+    old_graphics_button.click()
+    palatalization_check.click()
+    convert_button.click()
+    time.sleep(1)
+
+    assert (
+        output_field.get_attribute("value")
+        == "Chiba ż heta szpakoŭnia? Smiech dy j hodzie!"
     )
