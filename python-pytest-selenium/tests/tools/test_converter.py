50dab3dc0a4e0690833894c333aa6bf069e64485
Andrej Zacharevicz <andrej@zahar.ws>
2025-04-21 17:47:54 +0200

    Add tests on cleaning input field

--- python-pytest-selenium/tests/tools/test_converter.py
+++ python-pytest-selenium/tests/tools/test_converter.py
@@ -5,7 +5,9 @@ from typing import Iterator
 
 from pytest import fixture
 from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 from lib.config import BASE_URL
 
@@ -141,3 +143,37 @@ def test_convert_to_old_graphics_without_palatalization(driver: webdriver.Chrome
         output_field.get_attribute("value")
         == "Chiba ż heta szpakoŭnia? Smiech dy j hodzie!"
     )
+
+
+def test_clean_input_field(driver: webdriver.Chrome):
+    """Test button that should clean the input field.
+
+    Args:
+        webdriver.Chrome: Chrome driver object
+    """
+    input_field = driver.find_element(by=By.ID, value="input")
+    input_field.send_keys("Хіба ж гэта шпакоўня? Смех ды й годзе!")
+    clean_input_button = driver.find_element(
+        by=By.CSS_SELECTOR, value='button[title="Выдаліць тэкст"]'
+    )
+    time.sleep(1)
+
+    clean_input_button.click()
+    assert input_field.get_attribute("value") == ""
+
+
+def test_clean_input_field_hotkey(driver: webdriver.Chrome):
+    """Test hotkey that should clean the input field.
+
+    Args:
+        webdriver.Chrome: Chrome driver object
+    """
+    input_field = driver.find_element(by=By.ID, value="input")
+    input_field.send_keys("Хіба ж гэта шпакоўня? Смех ды й годзе!")
+    time.sleep(1)
+
+    chain_action = ActionChains(driver)
+    chain_action.key_down(Keys.CONTROL).key_down(Keys.DELETE).key_up(
+        Keys.CONTROL
+    ).key_up(Keys.DELETE).perform()
+    assert input_field.get_attribute("value") == ""
